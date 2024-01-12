bf7d404ca35539d5b5ac03fcf866c43e58de8d53
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-01-12 21:48:52 +0530

    added a trailing cursor and landing page

--- script.js
+++ script.js
@@ -22,18 +22,18 @@ document.getElementById("hamburger").addEventListener("click", function () {
   switchPanel();
 });
 
-document.body.addEventListener("click", function(event) {
-  if(event.target.tagName.toLowerCase() === 'a') {
-    const targetId = event.target.getAttribute('href').slice(1);
+document.body.addEventListener("click", function (event) {
+  if (event.target.tagName.toLowerCase() === "a") {
+    const targetId = event.target.getAttribute("href").slice(1);
     const targetElement = document.getElementById(targetId);
-    if(targetElement) {
+    if (targetElement) {
       event.preventDefault();
       targetElement.scrollIntoView();
     }
   }
 });
 
-function switchPanel(){
+function switchPanel() {
   if (!panelFlag) {
     document.getElementById("panel").style.transform = "translateX(-35%)";
     document.querySelector("#hamburger").style.backgroundColor = "black";
@@ -56,6 +56,81 @@ function switchPanel(){
     panelFlag = false;
   }
 }
-document.getElementById("overlay").addEventListener("click",()=>{
+document.getElementById("overlay").addEventListener("click", () => {
   switchPanel();
 });
+
+const cursor = document.getElementById("cursor");
+cursor.style.width = "20px";
+cursor.style.height = "20px";
+cursor.style.opacity = 0;
+document.addEventListener("mousemove", (e) => {
+  let cursorWidth = document.getElementById("cursor").style.width;
+  cursor.style.opacity = "1";
+  let xAxis = e.clientX;
+  let yAxis = e.clientY;
+  let shift = Number(cursorWidth.slice(0, -2)) * 0.5;
+  cursor.style.transform = `translate(${xAxis - shift }px, ${yAxis - shift}px)`;
+});
+
+document.querySelectorAll(".links").forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    cursor.style.width = 0;
+    cursor.style.height = 0;
+  });
+  link.addEventListener("mouseout", () => {
+    cursor.style.width = "20px";
+    cursor.style.height = "20px";
+  });
+});
+
+function increaseCursorSize(link,size) {
+  link.addEventListener("mouseover", () => {
+    cursor.style.height = `${size}px`;
+    cursor.style.width = `${size}px`;
+  });
+  link.addEventListener("mouseout", () => {
+    cursor.style.height = "30px";
+    cursor.style.width = "30px";
+  });
+}
+const logo = document.getElementById("logo");
+increaseCursorSize(logo,100);
+
+document.addEventListener("mouseout",()=>{
+  cursor.style.opacity = 0;
+})
+
+const homeContainer = document.getElementById("home-container");
+homeContainer.addEventListener("mouseover",()=>{
+  increaseCursorSize(homeContainer,150);
+})
+
+// const nameText = document.getElementById("name");
+// nameText.addEventListener("mouseover",()=>{
+//   increaseCursorSize(nameText,200);
+// })
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const myName = document.getElementById("name");
+
+function rnd2() {
+  let iteration = 0;
+  const interval = setInterval(() => {
+    myName.innerText = myName.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return myName.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+    if (iteration >= myName.dataset.value.length) {
+      clearInterval(interval);
+    }
+    iteration += 1 / 8;
+  }, 30);
+}
+ 
+rnd2();
